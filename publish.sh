a6c0a5c3e8a6e0d23a0a83a734e72e51cb1542b3
fabiobo2005 <fabiobo@msn.com>
2021-11-28 17:13:15 -0300

    update docker user and password secrets

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo $DOCKER_REGISTRY_PASSWORD | docker login -u $DOCKER_REGISTRY_USER ghcr.io --password-stdin
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME ghcr.io --password-stdin
 docker-compose -f ./iac/docker-compose.yml push
