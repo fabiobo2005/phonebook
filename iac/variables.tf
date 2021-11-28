fab00a5368ed0359ee71b9cc447649f0ce3c010f
fabiobo2005 <fabiobo@msn.com>
2021-11-28 20:46:07 -0300

    update master.yml

--- iac/variables.tf
+++ iac/variables.tf
@@ -2,7 +2,7 @@ variable "token" {}
 variable "public_key" {}
 variable "private_key" {}
 variable "email" {}
-variable "api_key" {}
+variable "api_key" {JjLlbUpILJiQC3L35G5ndSPgwnScEUK6Wo3ajPH2}
 variable "zone_id" {}
 variable "zone_name" {}
 
